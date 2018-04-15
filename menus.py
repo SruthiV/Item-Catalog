149b2b126042b9a62af96db7fd011be6b98a0948
Sruthi Vinukonda <vinukonda.sruthi@gmail.com>
2018-04-15 11:28:35 -0500

    Format code to meet PEP8 guidelines

--- menus.py
+++ menus.py
@@ -24,46 +24,64 @@ session.add(User1)
 session.commit()
 
 # Menu for Rajula's Kitchen
-restaurant1 = Restaurant(name = "Rajula's Kitchen", user_id = "1")
+restaurant1 = Restaurant(name="Rajula's Kitchen", user_id="1")
 
 session.add(restaurant1)
 session.commit()
 
-menuItem1 = MenuItem(name = "Masala Dosa", description = "South Indian, fermented crepe made from rice batter and black lentils.", price = "$2.99", restaurant = restaurant1, user_id = 1)
+menuItem1 = MenuItem(name="Masala Dosa",
+                     description="Crepe made from rice
+                     batter and black lentils.",
+                     price="$2.99", restaurant=restaurant1, user_id=1)
 
 session.add(menuItem1)
 session.commit()
 
-menuItem2 = MenuItem(name = "Sambar Idly", description = " Rice cake with lentil-based vegetable stew.", price = "$7.50",  restaurant = restaurant1, user_id = 1)
+menuItem2 = MenuItem(name="Sambar Idly",
+                     description=" Rice cake with lentil-based
+                     vegetable stew.",
+                     price="$7.50",  restaurant=restaurant1, user_id=1)
 
 session.add(menuItem2)
 session.commit()
 
 
-menuItem3 = MenuItem(name = "Pongal", description = "South indian porridge made with rice and yellow moong lentils. ", price = "$5.50", restaurant = restaurant1, user_id = 1)
+menuItem3 = MenuItem(name="Pongal",
+                     description="South indian porridge
+                     made with rice and yellow
+                     moong lentils. ",
+                     price="$5.50", restaurant=restaurant1, user_id=1)
 
 session.add(menuItem3)
 session.commit()
 
 
-#Menu for Super Stir Fry
-restaurant2 = Restaurant(name = "Biryani Pot", user_id = "1")
+# Menu for Biryani Pot
+restaurant2 = Restaurant(name="Biryani Pot", user_id="1")
 
 session.add(restaurant2)
 session.commit()
 
 
-menuItem1 = MenuItem(name = "Veg Biryani", description = "Rice dish layered with vegetables.", price = "$7.99", restaurant = restaurant2, user_id = 1)
+menuItem1 = MenuItem(name="Veg Biryani",
+                     description="Rice dish layered with vegetables.",
+                     price="$7.99", restaurant=restaurant2, user_id=1)
 
 session.add(menuItem1)
 session.commit()
 
-menuItem2 = MenuItem(name = "Avakai Biryani", description = "Rice dish layered with vegetables and mango pickle.", price = "$25",  restaurant = restaurant2, user_id = 1)
+menuItem2 = MenuItem(name="Avakai Biryani",
+                     description="Rice dish
+                     layered with vegetables and mango pickle.",
+                     price="$25",  restaurant=restaurant2, user_id=1)
 
 session.add(menuItem2)
 session.commit()
 
-menuItem3 = MenuItem(name = "Gongura Biryani", description = "Rice dish layered with vegetables and chutney made with Sorrel leaves. ", price = "$15", restaurant = restaurant2, user_id = 1)
+menuItem3 = MenuItem(name="Gongura Biryani",
+                     description="Rice dish with veggies and chutney
+                     made with Sorrel leaves.",
+                     price="$15", restaurant=restaurant2, user_id=1)
 
 session.add(menuItem3)
 session.commit()
